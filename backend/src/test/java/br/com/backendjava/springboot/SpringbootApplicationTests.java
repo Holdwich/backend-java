0e09eed4a01e7e4dc565212e95fb66554723b932
Holdwich <matheusflcastro@hotmail.com>
2024-12-30 13:53:47 -0300

    Início de projeto (backend)

---
+++ backend/src/test/java/br/com/backendjava/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backendjava.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
