39b24ddcf6a74724c097fb223a72a25b73927df7
Holdwich <matheusflcastro@hotmail.com>
2024-12-31 15:49:36 -0300

    adicionando classe DataSeeder para inicialização de usuários e alterando credenciais de conexão no Hibernate devido a erros de compatibilidade

---
+++ backend/src/main/java/br/com/backendjava/springboot/component/DataSeeder.java
@@ -0,0 +1,55 @@
+package br.com.backendjava.springboot.component;
+
+import javax.annotation.PostConstruct;
+
+import org.hibernate.Session;
+import org.springframework.stereotype.Component;
+
+import br.com.backendjava.springboot.HibernateUtil;
+import br.com.backendjava.springboot.model.UserModel;
+import br.com.backendjava.springboot.service.EncryptionService;
+
+// -- usuários iniciais (ocultar melhor em produção) --
+
+@Component
+public class DataSeeder {
+
+    @PostConstruct
+    public void seed(){
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try{
+
+            // User Admin
+
+            UserModel userAdmin = new UserModel();
+
+            userAdmin.setUsername("admin");
+            userAdmin.setPassword(EncryptionService.encrypt("123qwe!@#"));
+            userAdmin.setIsAdmin(true);
+
+            // User Client
+
+            UserModel userClient = new UserModel();
+
+            userClient.setUsername("user");
+            userClient.setPassword(EncryptionService.encrypt("123qwe123"));
+            userClient.setIsAdmin(false);
+
+            session.beginTransaction();
+
+            session.persist(userAdmin);
+            session.persist(userClient);
+
+            session.getTransaction().commit();
+        
+        } catch (Exception e){
+            if (session.getTransaction() != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            HibernateUtil.shutdown();
+        }
+    }
+}
