6efc0fa4404762ecc7fc68567b13be24ef40d948
Holdwich <matheusflcastro@hotmail.com>
2025-01-01 16:40:36 -0300

    removendo classes não utilizadas

--- backend/src/main/java/br/com/backendjava/springboot/config/SecurityConfig.java
+++ backend/src/main/java/br/com/backendjava/springboot/config/SecurityConfig.java
@@ -2,10 +2,8 @@ package br.com.backendjava.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
